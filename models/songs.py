0cc02c62af8a472ca84764eb78a7eab9bbd62c03
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2020-05-26 00:42:51 +0200

    Improved song model

--- models/songs.py
+++ models/songs.py
@@ -5,7 +5,7 @@ from mongoengine import Document, StringField, IntField
 
 class Songs(Document):
     """
-    Template for a mongoengine document, which represents a user's favorite meal.
+    Template for a mongoengine document
     :param artist: required string value, fewer than 50 characters
     :param title: required string value, fewer than 100 characters
     :param difficulty: optional int value, from 1 to 5
@@ -15,13 +15,11 @@ class Songs(Document):
     >>> from app import default_config
     >>> mongoengine.connect(**default_config['MONGODB_SETTINGS'])
     MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True, read_preference=Primary())
-    >>> new_song = Songs(artist= "Guthrie Govan", \
-                        title= "Remember when", \
-                        difficulty= 5)
+    >>> new_song = Songs(artist="Guthrie Govan", title="Remember when", difficulty=5)
     >>> new_song.save()
-    <Song: Song object>
+    <Songs: Songs object>
     """
 
     artist = StringField(required=True, max_length=50)
-    description = StringField(required=True, max_length=240)
+    title = StringField(required=True, max_length=240)
     difficulty = IntField(min_value=1, max_value=5)
